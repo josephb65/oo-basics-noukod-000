6eff124d5616ae46fd1a08539b9c526e97a53fac
Billy Joseph <ingbillyjoseph@gmail.com>
2019-04-23 19:29:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
  
@@ -9,4 +10,12 @@ def initialize(brand)
   self.condition = "new"
    puts "Your shoe is as good as new!"
  end 
+=======
+class Book
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :condition
+ 
+
+ 
+>>>>>>> cb602355920401cb27ed0283a3af884d3d0dce56
 end
